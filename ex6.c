6970e8759751057772e10d13be1de11b1fc667ee
pgg <jzi@inbox.ru>
2020-01-16 17:28:53 +0300

    formated ex6.c

--- ex6.c
+++ ex6.c
@@ -13,34 +13,33 @@ test_thread (void *v_param)
 int
 main (void)
 {
-  unsigned long count;
+   unsigned long count;
 
-  setvbuf (stdout, NULL, _IONBF, 0);
+   setvbuf (stdout, NULL, _IOLBF, 0);
 
-  for (count = 0; count < 2000; ++count)
-    {
+   for (count = 0; count < 100; ++count)
+   {
       pthread_t thread;
       int status;
 
       status = pthread_create (&thread, NULL, test_thread, NULL);
       if (status != 0)
-	{
-	  printf ("status = %d, count = %lu: %s\n", status, count,
-		  strerror (errno));
-	  return 1;
-	}
-      else
-	{
-	  printf ("count = %lu\n", count);
-	}
+      {
+         printf ("status = %d, count = %lu: %s\n", status, count,
+              strerror (errno));
+         return 1;
+      } else {
+         printf ("count = %lu\n", count);
+      }
       /* pthread_detach (thread); */
       int err = pthread_join (thread, NULL);
       if (err != 0)
-	{
-	  printf ("join failed (%s), count %lu\n", strerror (err), count);
-	  return 2;
-	}
-      usleep (10);
-    }
-  return 0;
+      {
+         printf ("join failed (%s), count %lu\n",
+              strerror (err), count);
+         return 2;
+      }
+   usleep (10);
+   }
+   return 0;
 }
